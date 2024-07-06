600946ec35541874145e57952c44b12a13a7de6e
Aureatus <93552500+Aureatus@users.noreply.github.com>
2024-07-06 15:41:55 +0100

    finish Games: Menu exercise

--- lib/games.ex
+++ lib/games.ex
@@ -1,18 +1,31 @@
 defmodule Games do
-  @moduledoc """
-  Documentation for `Games`.
-  """
+  def main(_args) do
+    input =
+      IO.gets("""
+      What game would you like to play?
+      1. Guessing Game
+      2. Rock Paper Scissors
+      3. Wordle
 
-  @doc """
-  Hello world.
+      Enter "stop" to exit.
+      """)
+      |> String.trim()
 
-  ## Examples
+    choice =
+      cond do
+        String.contains?(input, ["Guessing", "Game", "1"]) -> :guessing_game
+        String.contains?(input, ["Rock", "Paper", "Scissors", "2"]) -> :rock_paper_scissors
+        String.contains?(input, ["Wordle", "3"]) -> :wordle
+        input == "stop" -> :stop
+      end
 
-      iex> Games.hello()
-      :world
+    case choice do
+      :guessing_game -> Games.GuessingGame.play()
+      :rock_paper_scissors -> Games.RockPaperScissors.play()
+      :wordle -> Games.Wordle.play()
+      :stop -> :stop
+    end
 
-  """
-  def hello do
-    :world
+    unless choice == :stop, do: main([])
   end
 end
